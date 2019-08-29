7055ff4fab997a5353eb8e783824ff28e7d58d0b
Lidya Tsegay <lidyatsegay@outlook.com>
2019-08-29 16:05:38 -0400

    Add project files.

---
+++ GenericList/Genericlist.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericList {
+    class Genericlist {
+
+        //5930    7643    6168    9491    5296    6762    7554    1751    9037    8939    9206    2021
+
+        
+    }
+}
