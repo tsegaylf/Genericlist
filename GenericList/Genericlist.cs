21a392574833407900d40b1c4fe125a99e6523d9
Lidya Tsegay <lidyatsegay@outlook.com>
2019-08-29 16:30:32 -0400

    initial commit

--- GenericList/Genericlist.cs
+++ GenericList/Genericlist.cs
@@ -4,9 +4,5 @@ using System.Text;
 
 namespace GenericList {
     class Genericlist {
-
-        //5930    7643    6168    9491    5296    6762    7554    1751    9037    8939    9206    2021
-
-        
     }
 }
--- GenericList/Program.cs
+++ GenericList/Program.cs
@@ -1,9 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GenericList {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
+            var numbers = new List<int>();
+            numbers.Add(5930);
+            numbers.Add(7643);
+            numbers.Add(6168);
+            numbers.Add(9491);
+            numbers.Add(5296);
+            numbers.Add(6762);
+            numbers.Add(7554);
+            numbers.Add(1751);
+            numbers.Add(9037);
+            numbers.Add(8939);
+            numbers.Add(9206);
+            numbers.Add(2021);
+
+            var min = int.MaxValue;
+            var max = int.MinValue;
+            foreach (var nbr in numbers) {
+                if (nbr < min) { min = nbr;  }
+                if (nbr < min) { min = nbr;  }
+            }
+            Console.WriteLine($"Min is {min} and max ia {max}");
+
+            /// short answer
+            /// munbers.sort();
+            /// min = numbers[0];
+            /// max = numbers[number.Count -1];
+            /// Consol.Writleline($"Min is {min} and max is {max}");
         }
     }
 }
